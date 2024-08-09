75e1bdfbff499f8048030e5c0f175b9cc6d03ffd
Alice <you@example.com>
2024-08-09 20:08:41 +0900

    add service account user permission

--- cloudbuild_trigger.tf
+++ cloudbuild_trigger.tf
@@ -3,11 +3,11 @@ resource "google_service_account" "cloudbuild_sa" {
   project    = var.project_id
 }
 
-# resource "google_project_iam_member" "cloudbuild_sa_service_account" {
-#   project = var.project_id
-#   role    = "roles/iam.serviceAccountUser"
-#   member  = "serviceAccount:${google_service_account.cloudbuild_sa.email}"
-# }
+resource "google_project_iam_member" "cloudbuild_sa_service_account" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.cloudbuild_sa.email}"
+}
 
 resource "google_project_iam_member" "cloudbuild_sa_log_writer" {
   project = var.project_id
